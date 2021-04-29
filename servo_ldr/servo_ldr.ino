7e7da17cb455fd7b107005264400b8074a48e05d
Alison Venancio <alison.venancio@gmail.com>
2021-04-28 21:18:50 -0400

    Remove LCD from source

--- servo_ldr/servo_ldr.ino
+++ servo_ldr/servo_ldr.ino
@@ -1,6 +1,4 @@
 #include <Servo.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
 #define PIN_LDR_LFT 1
 #define PIN_LDR_RGHT 0
@@ -19,32 +17,16 @@ int ldrRght;
 int ldr_diff;
 int ang;
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
-
 void setup(){
     myServo.attach(PIN_SERVO);
     myServo.write(MIN_ANG);
     ang=MIN_ANG;
-
-    lcd.begin(20,4);
 }
 
 void loop(){
     ldrLft = analogRead(PIN_LDR_LFT);
     ldrRght = analogRead(PIN_LDR_RGHT);
     ldr_diff = ldrRght-ldrLft;
-
-    lcd.clear();
-    lcd.setCursor(0,1);
-    lcd.print("ldr1:");
-    lcd.setCursor(5,1);
-    lcd.print(ldrLft);
-
-    lcd.setCursor(0,2);
-    lcd.print("ldr2:");
-    lcd.setCursor(5,2);
-    lcd.print(ldrRght);
-    
 
     if(ldr_diff > DIFF_OFFSET) ang+=INC_VALUE;
     if(ang>MAX_ANG) ang=MAX_ANG;
